eefe629ba87b0a964984f1c4b55e39edee0d8a64
YuliaS <ysolodukha@gmail.com>
2022-12-27 12:12:17 +0200

    new commit

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -13,7 +13,7 @@ const Nav = () => {
                     <NavLink key={idx} navLinkId={navLinkId}  navLinkName={navLinkName} scrollToId={scrollToId} />
                 ))}
             </ul>
-            <Link className="menu-cv-btn btn" to={{ pathname: "https://docs.google.com/document/d/1Ys-bI8Von-JpapqLht0nV9Ws3-vkOHAe5QoyS955IXc/edit?usp=sharing" }} target="_blank">CV DOWNLOAD</Link>
+            <Link className="menu-cv-btn btn" to={{ pathname:"https://docs.google.com/document/d/1Ys-bI8Von-JpapqLht0nV9Ws3-vkOHAe5QoyS955IXc/edit?usp=sharing" }} target="_blank">CV DOWNLOAD</Link>
         </section>
 	);
 };
